ecdf40daa2f7381a688a48f13a20a815e7a9964a
Kécia Santos <keciakaroline@gmail.com>
2021-12-30 18:07:55 +0100

    Added HTML part

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export default function App() {
     <div className="App">
       <div className="container">
         {" "}
-        <h1>Weather App-Project</h1>
         <Weather />
         <footer>
           This project was coded by{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,47 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather"> Hello from Weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col col-sm-8">
+            <input
+              type="search"
+              placeholder="Search a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col col-sm-2">
+            <button type="submit" className="btn btn-light w-100">
+              {" "}
+              Search{" "}
+            </button>
+          </div>
+          <div className="col col-sm-2">
+            <button type="submit" className="btn btn-light w-100">
+              GPS{" "}
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="row">
+        <h2>City</h2>
+        <h3>Last updated:</h3>
+        <div className="col col-sm-6">
+          <img src="" alt="" />
+          <span>Temperature °C | °F</span>
+        </div>
+        <div className="col col-sm-6">
+          <ul>
+            <li>Description</li>
+            <li>Precipitation</li>
+            <li>Humidity</li>
+            <li>Wind</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
