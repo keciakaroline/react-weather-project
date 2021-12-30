e76301635e782e29505afe06b9a2300a9d0bab8c
Kécia Santos <keciakaroline@gmail.com>
2021-12-30 15:44:25 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App-Project</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather App-Project</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://portfolio-keciasantos.netlify.app/">Kécia Santos</a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/keciasantos/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
