01d140190b22b30fde17fd0a331b35c614800b7e
Kécia Santos <keciakaroline@gmail.com>
2021-12-30 17:44:39 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App-Project</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://portfolio-keciasantos.netlify.app/">Kécia Santos</a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/keciasantos/react-weather-project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        {" "}
+        <h1>Weather App-Project</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://portfolio-keciasantos.netlify.app/">Kécia Santos</a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/keciasantos/react-weather-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
