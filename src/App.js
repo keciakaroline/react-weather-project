f7d646e363af45dbb4027dfaf4c8dbeee6915db9
Kécia Santos <keciakaroline@gmail.com>
2022-06-15 01:11:43 +0200

    Created API usage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import TopCities from "./components/TopCities";
 import FormCity from "./components/FormCity";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import Forecast from "./components/Forecast";
+import getWeatherData from "./components/app/Api";
+import getFormattedWeatherData from "./components/app/Api";
+import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
 
 export default function App() {
+  const [location, setLocation] = useState({ q: "Lisbon" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...location, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+    fetchWeather();
+  }, [location, units]);
+
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl">
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopCities />
-      <h1> Teste </h1>
+
       <FormCity />
-      <TimeAndLocation />
-      <TemperatureAndDetails />
+
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
+      <Footer />
     </div>
   );
 }
